cdd232e2899a9328e71d18b54fe7d5d177eca02c
Austin Mason <austinpmason@gmail.com>
2016-02-24 21:29:16 +0000

    update structure

--- mastermind.rb
+++ mastermind.rb
@@ -15,12 +15,20 @@
 
 #classes
 ##player
+####name
 ##pegs
+####colour
 ##board
+####codemaker
 ##game
 
 module Mastermind
+	#Player needs to keep track of how many wins they have.
 	class Player
+		attr_reader :name
+		def initialize(name)
+			@name = name
+		end
 	end
 
 	class Board
