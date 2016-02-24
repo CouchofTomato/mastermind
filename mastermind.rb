8995b83b2d4eb94cf21c34358e8e4b43fdbcd687
Austin Mason <austinpmason@gmail.com>
2016-02-24 21:11:55 +0000

    create outline of game module

---
+++ mastermind.rb
@@ -0,0 +1,34 @@
+# A game that can be played by two human players or against a computer on the command line.
+# The board is a 4 X 12 Grid. In a two player game players alternate turns until one
+# player is victorious.
+#
+#One player becomes the codemaker, the other the codebreaker. The codemaker chooses a 
+#pattern of four code pegs. Duplicates are allowed, so the player could even choose four 
+#code pegs of the same color.
+
+#The codebreaker tries to guess the pattern, in both order and color, within twelve turns. 
+#Each guess is made by placing a row of code pegs on the decoding board. Once placed, the 
+#codemaker provides feedback by placing from zero to four key pegs in the small holes of 
+#the row with the guess. A colored or black key peg is placed for each code peg from the 
+#guess which is correct in both color and position. A white key peg indicates the existence 
+#of a correct color code peg placed in the wrong position.
+
+#classes
+##player
+##pegs
+##board
+##game
+
+module Mastermind
+	class Player
+	end
+
+	class Board
+	end
+
+	class Peg
+	end
+
+	class Game
+	end
+end
